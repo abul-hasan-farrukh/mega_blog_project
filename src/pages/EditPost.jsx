8175358c4db8c019fbd90079fe3597a9ab481fa6
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-09 22:46:38 +0530

    add entire project

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+import {Container, PostForm} from '../components'
+import appwriteService from "../appwrite/config";
+import { useNavigate,  useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [post, setPosts] = useState(null)
+    const {slug} = useParams() // using useParams to extract value from the URL to edit the post
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) { //if slug exists
+            appwriteService.getPost(slug).then((post) => {
+                if (post) {
+                    setPosts(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate]) //passing slug and navigate in dependency array beacuse changes will happen in slug and navigation
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ) : null
+}
+
+export default EditPost
