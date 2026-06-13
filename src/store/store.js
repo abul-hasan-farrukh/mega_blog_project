ba1d90886ce40a70289cc799ac6fac9c2ff90d00
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-06-13 21:33:50 +0530

    improved comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authSlice from './authSlice';
 
+// store needs information about all the reducers
 const store = configureStore({
     reducer: {
         auth : authSlice,
